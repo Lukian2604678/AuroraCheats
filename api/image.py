6db439c08bc4dabf07e925b841be4c71c4bf55b3
Lukian2604678 <maximnadtochiy3@gmail.com>
2025-07-10 18:55:57 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,9 +27,9 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False, # Enable the custom message?
         "message": "", # Message to show
-        "richMessage": False, # Enable rich text? (See README for more info)
+        "richMessage": True, # Enable rich text? (See README for more info)
     },
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
